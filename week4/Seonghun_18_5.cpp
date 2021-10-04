8443f7ab86d740c4b578d88f8736085868287ae8
seonghun <dev.seonghun@gmail.com>
2021-10-05 03:53:19 +0900

    Seonghun_18.5.cpp done

--- week4/Seonghun_18_5.cpp
+++ week4/Seonghun_18_5.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 #include <list>
 
+auto savelifenumber(int n, int k) {
+    std::list<int> savelife;
+    for(int i=1; i<=n; i++) savelife.push_back(i);
+    auto killpoint = savelife.begin();
+    while (savelife.size() > 2) {
+        if((killpoint = savelife.erase(killpoint)) == savelife.end()) killpoint = savelife.begin();
+        for (int i = 0; i < k-1 ; i++) {
+            if(++killpoint == savelife.end()) killpoint = savelife.begin();
+        }
+    }
+    return savelife;
+}
+
+int main() {
+    auto result = savelifenumber(6, 3);
+    std::cout << result.front() << " " <<result.back() << std::endl;
+}
 

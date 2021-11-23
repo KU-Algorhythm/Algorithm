7d7bb7b170366ee0d84b057cbdedcd7aecaa53f1
minjujuu <79794414+minjujuu@users.noreply.github.com>
2021-11-23 12:19:09 +0900

    Add files via upload

---
+++ week10/MJ_RS_kakao20_lv2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void)
+{
+    string s = "abcabcdede";
+
+    string curT, nextT, result;
+    
+    int count = 1;
+    int len = s.length();
+    int answer = len;
+    
+    for(int unit=1; unit<=(len/2); unit++)
+    {
+        curT = s.substr(0, unit);
+
+        int i = unit;
+        while (true)
+        {
+            /* 예외: 남은 글자 수가 단위보다 적은 경우 */
+            if(i+unit > s.length())
+            {
+                if (count > 1)
+                    result.append(to_string(count));
+                result.append(curT);
+
+                for (int j = i; j < s.length(); j++)
+                    result.push_back(s[j]);
+                break;
+            }
+
+            nextT = s.substr(i, unit);
+
+            if (!curT.compare(nextT))
+            {
+                count++;
+            }
+            else
+            {
+                if (count > 1)
+                {
+                    result.append(to_string(count));
+                    count = 1;
+                }
+                
+                result.append(curT);
+                curT = nextT;
+            }
+            i += unit;
+        }
+
+        if(answer > result.length())
+            answer = result.length();   
+
+        result.clear();
+        count = 1;
+    }
+    
+    return answer;
+
+}
